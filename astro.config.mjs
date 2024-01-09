fa8598a70e4cde11d4dfaa29027cf339c87a4924
impossible98 <christoleavenow@outlook.com>
2024-01-09 12:03:05 +0800

    feat: server-side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
+import netlify from "@astrojs/netlify";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: netlify()
+});
